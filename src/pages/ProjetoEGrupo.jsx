6df684bfc190751f69039e5e171ba0b248355f18
MARIN-Zenin <marin.lucas009@gmail.com>
2025-11-27 11:39:08 -0300

    Adicionando componentes às páginas

--- src/pages/ProjetoEGrupo.jsx
+++ src/pages/ProjetoEGrupo.jsx
@@ -1,6 +1,13 @@
+import "../App.css";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
 function ProjetoEGrupo() {
   return (
     <>
+    <Header/>
+    
+    <Footer />
     </>
   );
 }
