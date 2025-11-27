94b6fda25b837c6a0eb4c32abfdb07bbdf402253
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-11-27 09:04:21 -0300

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import ProjetoEGrupo from "./pages/ProjetoEGrupo";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projeto-e-grupo" element={<ProjetoEGrupo />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/ProjetoEGrupo.jsx
@@ -0,0 +1,8 @@
+function ProjetoEGrupo() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default ProjetoEGrupo;
