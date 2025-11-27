4806f0c04e3587bee39f3ec7c97f7651b4890565
MARIN-Zenin <marin.lucas009@gmail.com>
2025-11-27 14:35:41 -0300

    complementando componentes

--- src/pages/ProjetoEGrupo.jsx
+++ src/pages/ProjetoEGrupo.jsx
@@ -1,12 +1,25 @@
 import "../App.css";
 import Header from "../components/Header/Header";
+import Nav from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 
 function ProjetoEGrupo() {
   return (
     <>
     <Header/>
-    
+    <Nav/>
+
+  <div>
+    <div className="img-canto">
+     <img src="" alt="" />
+    </div>
+
+    <div className="texto-centro">
+     <h1>sla</h1>
+    </div>
+  </div>
+
+
     <Footer />
     </>
   );
