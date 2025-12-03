c0d06d3c12f97311b1005553762960a6c9fa5eea
Ana <anabfariasp@gmail.com>
2025-12-03 14:55:37 -0300

    Navbar corrigido

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,17 @@
 import "./NavBar.css";
 
 export default function NavBar() {
-  return (
-    <>
-      <header className="topbar">
-        <div className="topbar-container">
-          <div className="topbar-left">
-            <img src="/logo.png" alt="Logo" className="logo-img" />
-            <h2 className="logo-text">Lumina</h2>
-          </div>
+    return (
+        <nav className="sidebar">
+            <div className="sidebar-logo">
+                <img src="logo.png" alt="Logo" />
+                <h2>Lumina</h2>
+            </div>
 
-          <h1 className="title">PLANNER DE AULAS</h1>
-
-          <div className="topbar-right">
-            <img src="/icon.png" alt="Ícone" className="icon-img" />
-          </div>
-        </div>
-      </header>
-
-      <nav className="navbar-menu">
-        <ul>
-          <li><a href="/index">HOME</a></li>
-          <li><a href="/projeto">PROJETO & GRUPO</a></li>
-        </ul>
-      </nav>
-    </>
-  );
+            <ul className="sidebar-menu">
+                <li><a href="/index.html">Home</a></li>
+                <li><a href="/projeto.html">Projeto & Grupo</a></li>
+            </ul>
+        </nav>
+    );
 }
